7e3f8bd62505b99a8c0c859d1a08ef6b060c375f
Ignacio Cafiero Torrubia <ignaciocafiero12@gmail.com>
2023-08-10 22:54:54 -0300

    Create adminUser

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,5 @@
 class AdminUser < ApplicationRecord
+  self.table_name = 'admin_users'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
--- db/migrate/20230810224711_devise_create_admin_users.rb
+++ db/migrate/20230810224711_devise_create_admin_users.rb
@@ -40,8 +40,7 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration[7.0]
     add_index :admin_users, :reset_password_token, unique: true
 
     # add_index :admin_users, :confirmation_token,   unique: true
-    # add_index :admin_users, :unlock_token,         unique: true
+    # add_index :admin_users, :unlock_token,         unique: tru
+    AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password")
   end
-
-  AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password") 
 end
